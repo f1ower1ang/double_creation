1135a72f8a25d8672d6b2fa3d8647a212b760374
flowerLang <m15779063830_1@163.com>
2020-03-23 13:57:23 +0800

    fix

--- src/views/Course/Course.vue
+++ src/views/Course/Course.vue
@@ -9,18 +9,30 @@
           span 作者：{{ creator }}
         p.info-desc(v-html="course.detail")
       section.common-content
-        h2.section-title 实验内容
+        h2.section-title 先学知识
+        div.section-points
+          ul
+            li.point Python基础语法
+            li.point 数据结构基础
+          ul
+            li.point jupyter基本使用
+            li.point 实验室基本使用
+      section.common-content
+        h2.section-title 实验环境
+        ul.bg-page.list
+          li 实验主机：CentOs 7
+          li IDE：jupyter Lab
     .course-page__right
-        .bg(:style="{backgroundImage: `url(${course.imgUrl})`}")
-        .wrapper
-          Button(type="primary" @click="addCourse" size="large" long) 开始上机
-          Button(type="text" size="small" long @click="addRate") 喜欢本实验？给个评分吧
-        p.btn(v-if="!favor" @click="toggleFav")
-          Icon(type="ios-star-outline")
-          span 收藏
-        p.btn(v-if="favor" @click="toggleFav")
-          Icon(type="ios-star")
-          span 已收藏
+      .bg(:style="{backgroundImage: `url(${course.imgUrl})`}")
+      .wrapper
+        Button(type="primary" @click="addCourse" size="large" long) 开始上机
+        Button(type="text" size="small" long @click="addRate") 喜欢本实验？给个评分吧
+      p.btn(v-if="!favor" @click="toggleFav")
+        Icon(type="ios-star-outline")
+        span 收藏
+      p.btn(v-if="favor" @click="toggleFav")
+        Icon(type="ios-star")
+        span 已收藏
     transition(name="fade")
       .run-loading(v-show="loading")
         .message
@@ -51,126 +63,130 @@ import 'tui-editor/dist/tui-editor-contents.css' // editor content
 import 'codemirror/lib/codemirror.css'
 import { addFavorite, addUserCourse, deleteFavorite } from '@/api/user' // codemirror
 
-@Component({
-  components: { AppIcon }
-})
+  @Component({
+    components: { AppIcon }
+  })
 export default class CourseDetail extends Vue {
-  private course: any = { title: '', id: '', time: '', date: '', detail: '' }
-  private mdToHtml: MdToHtml | null = null
-  private loading: Boolean = false
-  private courseUrl: string = ''
-  private modal: Boolean = false
-  private rateModal: Boolean = false
-  private rate: number = 0
-  private creator: String = ''
-  private collected: number = 0
+    private course: any = { title: '', id: '', time: '', date: '', detail: '' }
+    private mdToHtml: MdToHtml | null = null
+    private loading: Boolean = false
+    private courseUrl: string = ''
+    private modal: Boolean = false
+    private rateModal: Boolean = false
+    private rate: number = 0
+    private creator: String = ''
+    private collected: number = 0
 
-  get favor(): Boolean {
-    const index = this.favoriteList.findIndex((item: Course) => parseInt(item.id) === parseInt(this.$route.params.id))
-    return index > -1
-  }
-
-  @Getter token: string | undefined
-  @Getter userInfo: any
-  @Getter favoriteList: any
-
-  private mounted() {
-    this.mdToHtml = new MdToHtml('#md-text')
-    if (this.course.detail) {
-      this.mdToHtml.setValue(this.course.detail)
-      this.course.detail = this.mdToHtml.getHtml()
+    get favor(): Boolean {
+      const index = this.favoriteList.findIndex((item: Course) => parseInt(item.id) === parseInt(this.$route.params.id))
+      return index > -1
     }
-  }
 
-  private created() {
-    getCourseDetail(this.$route.params.id).then((res: any) => {
-      this.course = formatCourse(res.data.course)
-      this.creator = res.data.createrName
-      this.collected = res.data.userCollectionNumber
-      if (this.mdToHtml) {
+    @Getter token: string | undefined
+    @Getter userInfo: any
+    @Getter favoriteList: any
+
+    private mounted() {
+      this.mdToHtml = new MdToHtml('#md-text')
+      if (this.course.detail) {
         this.mdToHtml.setValue(this.course.detail)
         this.course.detail = this.mdToHtml.getHtml()
       }
-    })
-  }
+    }
 
-  private async addCourse() {
-    if (this.token) {
-      await addUserCourse(this.course.id)
-      this.loading = true
-      const res: any = await startContainer({ courseId: this.course.id, userId: this.userInfo.id })
-      if (res.errCode >= 200 && res.errCode < 300) {
-        const res: any = await checkContainer({ courseId: this.course.id, userId: this.userInfo.id })
-        if (res.code !== 200) {
-          let timer = setInterval(async() => {
-            const res: any = await checkContainer({ courseId: this.course.id, userId: this.userInfo.id })
-            if (res.code === 200) {
-              clearInterval(timer)
-              this.courseUrl = res.data.jumpUrl
-              this.loading = false
-              this.modal = true
-            }
-          }, 5000)
+    private created() {
+      getCourseDetail(this.$route.params.id).then((res: any) => {
+        this.course = formatCourse(res.data.course)
+        this.creator = res.data.createrName
+        this.collected = res.data.userCollectionNumber
+        if (this.mdToHtml) {
+          this.mdToHtml.setValue(this.course.detail)
+          this.course.detail = this.mdToHtml.getHtml()
+        }
+      })
+    }
+
+    private async addCourse() {
+      if (this.token) {
+        await addUserCourse(this.course.id)
+        this.loading = true
+        const res: any = await startContainer({ courseId: this.course.id, userId: this.userInfo.id })
+        if ((res.errCode >= 200 && res.errCode < 300) || (res.code >= 200 && res.code < 300)) {
+          const res: any = await checkContainer({ courseId: this.course.id, userId: this.userInfo.id })
+          if (res.code !== 200) {
+            let timer = setInterval(async() => {
+              const res: any = await checkContainer({ courseId: this.course.id, userId: this.userInfo.id })
+              if (res.code === 200) {
+                clearInterval(timer)
+                this.courseUrl = res.data.jumpUrl
+                this.loading = false
+                this.modal = true
+              }
+            }, 5000)
+          } else {
+            this.loading = false
+            this.courseUrl = res.data.jumpUrl
+            this.modal = true
+          }
         } else {
           this.loading = false
-          this.courseUrl = res.data.jumpUrl
-          this.modal = true
+          this.$Message.warning('启动失败，尝试刷新页面')
         }
       } else {
-        this.loading = false
-        this.$Message.warning('启动失败，尝试刷新页面')
+        this.$Message.warning('请先登录')
+        this.$router.push(`/login?redirect=${this.$route.path}`)
       }
-    } else {
-      this.$Message.warning('请先登录')
-      this.$router.push(`/login?redirect=${this.$route.path}`)
     }
-  }
-  private enterLab() {
-    this.modal = false
-    open(this.courseUrl)
-  }
-  private toggleFav() {
-    if (this.token) {
-      if (this.favor) {
-        deleteFavorite(this.course.id).then(res => {
-          this.$Message.success('取消收藏成功')
-          this.$store.dispatch('course/deleteFavorite', this.course)
-        })
+
+    private enterLab() {
+      this.modal = false
+      open(this.courseUrl)
+    }
+
+    private toggleFav() {
+      if (this.token) {
+        if (this.favor) {
+          deleteFavorite(this.course.id).then(res => {
+            this.$Message.success('取消收藏成功')
+            this.$store.dispatch('course/deleteFavorite', this.course)
+          })
+        } else {
+          addFavorite(this.course.id).then(res => {
+            this.$Message.success('收藏成功')
+            this.$store.dispatch('course/addFavorite', this.course)
+          })
+        }
       } else {
-        addFavorite(this.course.id).then(res => {
-          this.$Message.success('收藏成功')
-          this.$store.dispatch('course/addFavorite', this.course)
-        })
+        this.$Message.warning('请先登录')
+        this.$router.push(`/login?redirect=${this.$route.path}`)
       }
-    } else {
-      this.$Message.warning('请先登录')
-      this.$router.push(`/login?redirect=${this.$route.path}`)
     }
-  }
-  private addRate() {
-    if (this.token) {
-      this.rateModal = true
-    } else {
-      this.$Message.warning('请先登录')
-      this.$router.push(`/login?redirect=${this.$route.path}`)
-    }
-  }
-  private submitRate() {
-    commitRate({
-      courseid: this.course.id,
-      rate: this.rate
-    }).then((res: any) => {
-      if (res.errCode === 200) {
-        this.$Message.success('评分成功')
+
+    private addRate() {
+      if (this.token) {
+        this.rateModal = true
       } else {
-        this.$Message.warning(res.errMsg)
+        this.$Message.warning('请先登录')
+        this.$router.push(`/login?redirect=${this.$route.path}`)
       }
-    })
-    this.rateModal = false
-  }
+    }
+
+    private submitRate() {
+      commitRate({
+        courseid: this.course.id,
+        rate: this.rate
+      }).then((res: any) => {
+        if (res.errCode === 200) {
+          this.$Message.success('评分成功')
+        } else {
+          this.$Message.warning(res.errMsg)
+        }
+      })
+      this.rateModal = false
+    }
 }
 </script>
 
 <style scoped lang="scss">
-@import 'Course';
+  @import 'Course';
 </style>
